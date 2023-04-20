471dcff83ab5246f1ae53795fd020b3446e6a594
Noelia Cruz <112984139+NoeliaCush@users.noreply.github.com>
2023-04-19 23:23:57 -0300

    Add files via upload

---
+++ JPA-master/JPA_Ejemplo_CON_Herencia/build/generated-sources/ap-source-output/JPA_Ejemplo_1/entidades/Mascota_.java
@@ -0,0 +1,15 @@
+package JPA_Ejemplo_1.entidades;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-12-07T19:17:25")
+@StaticMetamodel(Mascota.class)
+public class Mascota_ { 
+
+    public static volatile SingularAttribute<Mascota, String> id;
+    public static volatile SingularAttribute<Mascota, String> nombre;
+    public static volatile SingularAttribute<Mascota, String> dni;
+
+}
